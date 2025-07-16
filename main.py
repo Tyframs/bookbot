7569772e8e15d01518ad37ef6a415028d477874a
Tyframs <tysupplee@protonmail.com>
2025-07-16 19:07:41 -0400

    initial python started, Lesson 6 Chapter 2

---
+++ main.py
@@ -0,0 +1,12 @@
+from stats import count_words
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    return get_book_text("books/frankenstein.txt")
+
+num_words = count_words(main())
+print(f"{num_words} words found in the document")

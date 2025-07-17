0a3b15a491537d058240ec501e6a7d132f8c3a95
Tyframs <tysupplee@protonmail.com>
2025-07-17 08:37:48 -0400

    Report created, next up is arguments.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_letters, sort_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,5 +8,20 @@ def get_book_text(filepath):
 def main():
     return get_book_text("books/frankenstein.txt")
 
+# print("============ BOOKBOT ============")
+# print("Analyzing book found at books/frankenstein.txt...")
+# print("----------- Word Count ----------")
+
 num_words = count_words(main())
-print(f"{num_words} words found in the document")
+print(f"Found {num_words} total words")
+
+# print("--------- Character Count -------")
+
+num_letters = count_letters(main())
+letters = sort_dict(count_letters(main()))
+filtered_letters = {letter['key']: letter['num'] for letter in letters if letter['key'].isalpha()}
+
+for k, v in filtered_letters.items():
+    print(f"{k}: {v}")
+
+# print("============= END ===============")
--- stats.py
+++ stats.py
@@ -2,3 +2,22 @@ def count_words(text):
     words = text.split()
     word_count = len(words)
     return word_count
+
+def count_letters(text):
+    words = text.lower()
+    letter_counts = {}
+    for line in words:
+        for letter in line:
+            if letter in letter_counts:
+                letter_counts[letter] +=1
+            else:
+                letter_counts[letter] = 1
+    return letter_counts
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(dict):
+    converted_dictlist = [{"key": l, "num": n} for l, n in dict.items()]
+    converted_dictlist.sort(reverse=True, key=sort_on)
+    return converted_dictlist
